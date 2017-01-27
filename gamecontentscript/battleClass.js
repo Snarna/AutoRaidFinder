63a75b67d706d394662c203493a5676303cbc46d
Snarna <xyq163446@gmail.com>
2017-01-26 22:11:02 -0500

    Working on observer

--- gamecontentscript/battleClass.js
+++ gamecontentscript/battleClass.js
@@ -3,7 +3,27 @@
 */
 
 var BattleClass = {
-
+  battleButton: null,
+  observer: null,
+  ini: function(){
+    this.battleButton = $(".btn-attack-start");
+    if(this.battleButton){
+      this.observer = new MutationObserver(BattleClass.buttonHandler);
+      var config = {
+        attributes: true,
+        attributeFilter: ["class"],
+        OldValue: true,
+        childList: true,
+        subtree: true
+      };
+      observer.observe(this.battleButton, config);
+    }
+  }
+  buttonHandler: function(mutations){
+    mutations.forEach(function(mutation){
+      console.log(mutation);
+    });
+  }
 }
 
 function setBattleClass(){
@@ -16,6 +36,7 @@ function startListen(){
   chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     if(msg.backCommand == 'startAutoAttack'){
       //Start Auto-attack
+      BattleClass.ini();
     }
   });
 }
